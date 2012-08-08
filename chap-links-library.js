8eb1b4608fd21f81f71dc6ebd91c61845e36fcdd
urmuzov <urmuzov@gmail.com>
2012-08-08 20:01:14 +0000

    chap-links-library stub

---
+++ chap-links-library.js
@@ -0,0 +1,15 @@
+/**
+ * @namespace
+ */
+var links;
+
+/**
+ * @constructor
+ * @param container
+ */
+links.Timeline = function(container){};
+links.Timeline.prototype.draw = function(data, options){};
+/**
+ * @param {Date} time
+ */
+links.Timeline.prototype.setCustomTime = function(time){};
